c576ffdecd2405ab0c017d06d7e1b4b80efe767c
bibarys <bibaryssultan@icloud.com>
2025-03-01 04:07:17 +0500

    Implement TodoList, Counter, ShoppingList, Profile with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import NameByLocalStorage from "./components/NameByLocalStorage";
+import Counter from "./components/Counter";
+import Profile from "./components/Profile";
+import ShoppingList from "./components/ShoppingList";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <div>
-      <NameByLocalStorage />
+      <Counter />
+      <TodoList />
+      <Profile />
+      <ShoppingList />
     </div>
   );
 };
