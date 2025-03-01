025b5a80df82a8be1df58af0386daf84bb548581
bibarys <bibaryssultan@icloud.com>
2025-03-01 20:27:59 +0500

    implement advance useContext hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from "react";
-import ComponentA from "./components/ComponentA";
-import { createContext } from "react";
-
-export const Data = createContext();
-export const Data1 = createContext();
+import UserProfile from "./components/UserProfile";
+import { UserProvider } from "./UserContext";
+import UpdateUser from "./components/UpdateUser";
 
 const App = () => {
-  const name = "John";
-  const age = 25;
   return (
-    <div>
-      <Data.Provider value={name}>
-        <Data1.Provider value={age}>
-          <ComponentA />
-        </Data1.Provider>
-      </Data.Provider>
-    </div>
+    <UserProvider>
+      <UserProfile />
+      <UpdateUser />
+    </UserProvider>
   );
 };
 
