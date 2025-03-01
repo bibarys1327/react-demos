b8a4ca607ecd039546da2a00cdd99cfc37256d6f
bibarys <bibaryssultan@icloud.com>
2025-03-01 05:11:40 +0500

    Implement useEffect fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import Switcher from "./components/Switcher";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      if (data && data.length) setData(data);
+    }
+    getData();
+  }, []);
   return (
     <div>
-      <Switcher />
+      <ul>
+        {data.map((todo) => (
+          <section key={todo.id}>
+            <li>{todo.title}</li>
+            <li>Body --- {todo.body}</li>
+          </section>
+        ))}
+      </ul>
     </div>
   );
 };
