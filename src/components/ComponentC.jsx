37be2e570edb04d06bab9816b63cc87c495a927a
bibarys <bibaryssultan@icloud.com>
2025-03-01 20:00:09 +0500

    Implement useContext

--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -1,27 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Data, Data1 } from "../App";
 
 const ComponentC = () => {
+  const username = useContext(Data);
+  const Age = useContext(Data1);
   return (
-    <Data.Consumer>
-      {(name) => {
-        {
-          /* return <h1>{name}</h1>; */
-        }
-
-        return (
-          <Data1.Consumer>
-            {(age) => {
-              return (
-                <h1>
-                  My name is: {name} and I'm {age} years old.
-                </h1>
-              );
-            }}
-          </Data1.Consumer>
-        );
-      }}
-    </Data.Consumer>
+    <h1>
+      My name is: {username} adm I'm {Age} years old
+    </h1>
   );
 };
 
