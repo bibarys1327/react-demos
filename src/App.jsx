018dd6b3f118b14ade729dc6a0e434dda406a1b2
bibarys <bibaryssultan@icloud.com>
2025-03-01 05:25:48 +0500

    Implement useEffect BasiEffect, CounterEffect, FetchDataEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import BasicEffect from "./components/BasicEffect";
+import CounterEffect from "./components/CounterEffect";
+import FetchDataEffect from "./components/FetchDataEffect";
 
 const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const data = await response.json();
-      if (data && data.length) setData(data);
-    }
-    getData();
-  }, []);
   return (
     <div>
-      <ul>
-        {data.map((todo) => (
-          <section key={todo.id}>
-            <li>{todo.title}</li>
-            <li>Body --- {todo.body}</li>
-          </section>
-        ))}
-      </ul>
+      <BasicEffect />
+      <CounterEffect />
+      <FetchDataEffect />
     </div>
   );
 };
